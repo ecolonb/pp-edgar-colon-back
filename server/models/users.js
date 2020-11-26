da318a93a10a60723336021c917f6d7b266c4124
Edgar Numina <edgar@numina.mx>
2020-11-25 18:52:47 -0600

    Adding user model

---
+++ server/models/users.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: [true, 'The name is reqired'],
+    min: [1, 'Min char 1'],
+    max: [100, 'Max char is 100']
+  },
+  lastName: {
+    type: String,
+    required: false,
+    max: [100, 'Max char is 100']
+  },
+  email: {
+    type: String,
+    required: [true, 'The email is reqired'],
+    unique: true
+  },
+  cellphone: {
+    type: String,
+    required: false
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  birthdate: {
+    type: Date,
+    required: [true, 'The password is reqired']
+  },
+  gender: {
+    type: String,
+    required: [true, 'The gender is reqired'],
+    enum: ['m', 'f']
+  },
+  hobby: {
+    type: String,
+    required: false
+  },
+  created_at: {
+    type: Date,
+    default: new Date(),
+    required: false
+  }
+});
+
+module.exports = model('User', UserSchema);
